6176636167db601b3bca8119665bbf9bf67c36e9
Jagmohan Kushwaha <jagmohan.jk11@gmail.com>
2026-07-14 03:58:15 +0530

    new changes

--- frontend/angular-app/src/app/core/services/vehicle.service.ts
+++ frontend/angular-app/src/app/core/services/vehicle.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class VehicleService {
-  private apiUrl = 'http://localhost:8080/api/users/vehicles';
+  private apiUrl = 'http://localhost:8080/api/vehicles';
 
   constructor(private http: HttpClient) { }
 
